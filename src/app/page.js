8e96e2928667b21e36acb7a8c10dd3d9fe4f6626
Raymond <32289508+Ubaton@users.noreply.github.com>
2024-10-31 19:51:03 +0200

    Load Google Analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Hero from "@/components/Home/Hero";
 import Features from "@/components/Home/Features";
@@ -5,8 +7,21 @@ import HowItWorks from "@/components/Home/HowItWorks";
 import GetStarted from "@/components/Home/GetStarted";
 import { Separator } from "@/components/ui/separator";
 import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Load Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, {
+      page_path: window.location.pathname,
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,23 +29,10 @@ export default function Home() {
         <meta name="description" content="Raymond Ngobeni" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-
         <script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
         ></script>
-        <script>
-          dangerouslySetInnerHTML=
-          {{
-            __html: ` window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); 
-          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}',{
-            page_path: window.location.pathname,
-            });
-            `,
-          }}
-        </script>
       </Head>
       <div className="flex flex-col justify-center items-center min-h-screen">
         <Hero />
