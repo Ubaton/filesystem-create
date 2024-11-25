18ede4a7c07c235dd1c9ed97af66f0ceb4d752d1
Raymond <32289508+Ubaton@users.noreply.github.com>
2024-11-25 15:20:45 +0200

    Ribbon at Corner of filegen main file

--- src/app/docs/license/page.js
+++ src/app/docs/license/page.js
@@ -8,10 +8,10 @@ const MITLicense = () => {
       <p className="mb-4">
         Permission is hereby granted, free of charge, to any person obtaining a
         copy of this software and associated documentation files (the
-        "Software"), to deal in the Software without restriction, including
-        without limitation the rights to use, copy, modify, merge, publish,
-        distribute, sublicense, and/or sell copies of the Software, and to
-        permit persons to whom the Software is furnished to do so, subject to
+        &quot;Software&quot;), to deal in the Software without restriction,
+        including without limitation the rights to use, copy, modify, merge,
+        publish, distribute, sublicense, and/or sell copies of the Software, and
+        to permit persons to whom the Software is furnished to do so, subject to
         the following conditions:
       </p>
       <p className="mb-4">
@@ -19,8 +19,8 @@ const MITLicense = () => {
         in all copies or substantial portions of the Software.
       </p>
       <p className="font-semibold mb-2">
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-        OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY
+        KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
         MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
         IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
         CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
