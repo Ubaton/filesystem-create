e394c3927a000be11991dce811a630fce57aa37a
Raymond <32289508+Ubaton@users.noreply.github.com>
2024-12-09 09:18:18 +0200

    web version update

--- src/components/Navbar/Navigation.js
+++ src/components/Navbar/Navigation.js
@@ -85,7 +85,7 @@ const Navigation = () => {
             </Badge>
           </Link>
           <Badge variant="outline" className="text-sm text-muted-foreground">
-            v0.2.3
+            v0.2.4
           </Badge>
           <Link
             href="https://github.com/Ubaton"
